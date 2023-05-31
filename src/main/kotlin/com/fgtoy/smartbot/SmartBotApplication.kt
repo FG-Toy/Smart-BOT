0af1a2553eb2cf113da3d715bbcfa75a1cb7cb03
Dongbin <dongbin.im@nhnfashiongo.com>
2023-05-31 18:16:05 +0900

    init project

---
+++ src/main/kotlin/com/fgtoy/smartbot/SmartBotApplication.kt
@@ -0,0 +1,11 @@
+package com.fgtoy.smartbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/fgtoy/smartbot/SmartBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fgtoy.smartbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
